b32f540ee1c3b7b1afbad28851508c3cc80ee28d
Kevin Delisle <delisle@ca.ibm.com>
2016-03-14 22:11:58 -0400

    load-config: create error types from config

---
+++ test/test-load-config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var _ = require('lodash');
+var handlerr = require('../index');
+var path = require('path');
+var tap = require('tap');
+
+tap.test('Error Configuration', function (t) {
+  t.test('Load error types from config', function(t) {
+    var config = handlerr.loadConfig(path.resolve('test', 'fixtures',
+      'fake-config.json'));
+    t.ok(config, 'Config has value');
+    _.each(config, function(err) {
+      t.ok(err.prototype.name, 'Error constructor has a name');
+      t.ok(err.prototype.code, 'Error constructor has a code');
+      try {
+        throw new err('errmsg');
+      } catch (err) {
+        t.equal(err.message, 'errmsg', 'error has correct message');
+      }
+    });
+    t.end();
+  });
+  t.end();
+});
